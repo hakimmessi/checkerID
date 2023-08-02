9d3b96f1270b7293f537fffe00b4fec6616759ca
Hakim Messi <ST10082700@vcconnect.edu.za>
2023-08-02 14:35:25 +0200

    Add project files.

---
+++ validationClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkerID
+{
+    public class validationClass
+    {
+        #region method to verify if the user's input as an ID number is correct and in the right format.
+        public bool isIDValid(string ID)
+        {
+            // check if the ID is null or empty or doesn't have exactly 13 characters.
+            if (string.IsNullOrEmpty(ID) || ID.Length != 13)
+            {
+                // return false as the ID is invalid.
+                return false; 
+            }
+
+            // extract different parts of the ID for further validation.
+            string dateOfBirthPart = ID.Substring(0, 6); 
+            string genderPart = ID.Substring(6, 4);
+            char citizenshipStatus = ID[10];
+            char randomDigit = ID[11];
+            char checksumDigit = ID[12];
+
+            // check the format of each part of the ID's number
+            bool validDateOfBirth = int.TryParse(dateOfBirthPart, out _);
+            bool validGender = int.TryParse(genderPart, out int genderNumber) && genderNumber >= 0 && genderNumber <= 9999;
+            bool validCitizenshipStatus = citizenshipStatus == '0' || citizenshipStatus == '1';
+            bool validRandomDigit = char.IsDigit(randomDigit);
+            bool validChecksumDigit = char.IsDigit(checksumDigit);
+
+            // return true only if all parts have the correct format
+            return validDateOfBirth && validGender && validCitizenshipStatus && validRandomDigit && validChecksumDigit;
+        }
+        #endregion
+
+        #region method to check if the user's input as a Passport number is in a valid format.
+
+        #endregion
+
+    }
+}
---
+++ workerClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkerID
+{
+    public  class workerClass
+    {
+        // dictionary to store vaccinated user data with their ID or Passport number as the key and vaccination number as the value.
+        Dictionary<string, int> vaccinatedUsers = new Dictionary<string, int>()
+        {
+            {"9202201234161", 23451},
+            {"9503156789070", 67890},
+            {"0105037253051", 48932},
+            {"M24587631", 89236},
+            {"L29865783", 90765}
+        };
+
+        
+    }
+}
+
+    
+
